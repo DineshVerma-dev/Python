0e892ef9d0526d0bf9844e75d1007b8725ee884d
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-02-15 11:05:47 +0530

    Python

---
+++ Basics.py
@@ -0,0 +1,13 @@
+# print("dineshverma")
+
+# for i in range(4) :
+#    print(i)
+
+# print(123)
+
+def chai(n):
+    print(n)
+
+chai(3)    
+chai("lemon tea")
+chai("hello python developer how arse you ")
---
+++ chai.py
@@ -0,0 +1,4 @@
+from Basics import chai
+
+chai("ginger adarak")
+chai("hello")
