c3cc57e90d12b9b7f13fa29986e3d73d23402197
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-02-15 23:32:12 +0530

    learning D1 PY

--- Basics.py
+++ Basics.py
@@ -11,3 +11,29 @@ def chai(n):
 chai(3)    
 chai("lemon tea")
 chai("hello python developer how arse you ")
+
+chai_one = "lemon"
+
+# immutable cannot change 
+# >>> x = 10
+# >>> y = x
+# >>> x
+# 10
+# >>> print("Address of x:", id(x))
+# Address of x: 140720343680200
+# >>> y
+# 10
+# >>> print("Address of y:", id(y))
+# Address of y: 140720343680200
+# >>> x = 20
+# >>> print("Address of x:", id(x))
+# Address of x: 140720343680520
+# >>> print("Address of y:", id(y))
+# Address of y: 140720343680200
+# >>> 
+
+import random
+random.choice([1,2,3,4,5,6,7,8,9])
+
+myD = {'one':'lemon','two':'hello'}
+print(myD)
